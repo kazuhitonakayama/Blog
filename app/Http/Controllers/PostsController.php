6af22bcc933dfae33ff56da0a37b48800cca56fd
kazuhito_nakayama <knakayama@valt.jp>
2020-01-29 17:06:21 +0900

    Having finished 12_"ooutdata to view"

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
     //
     public function index () {
-        return view('posts.index');
+       //$posts = Post::all();
+       //$posts = Post::orderBy('created_at','desc')->get();
+         $posts = Post::latest()->get();
+       //dd($posts->toArray());  //dump die の種類
+       //return view('posts.index',['posts'=>$posts]);
+        return view('posts.index')->with('posts',$posts);
     }
 }
